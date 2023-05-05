11b9242fc7a75e6df2ca8da0d5cff1dfd2472103
czlonkowski <56956555+czlonkowski@users.noreply.github.com>
2023-05-06 00:37:41 +0200

    dsa

--- run.py
+++ run.py
@@ -3,10 +3,12 @@ from server.website import Website
 from server.backend import Backend_Api
 
 from json import load
+import os
 
 if __name__ == '__main__':
     config = load(open('config.json', 'r'))
-    
+    site_config = config['site_config']
+
     site = Website(app)
     for route in site.routes:
         app.add_url_rule(
@@ -23,4 +25,10 @@ if __name__ == '__main__':
             methods=backend_api.routes[route]['methods'],
         )
 
-    app.run()
+    host = site_config.get('host', '0.0.0.0')
+    port = int(os.getenv("PORT", site_config.get('port', 5000)))
+    debug = site_config.get('debug', False)
+
+    print(f"Running on {host}:{port}")
+    app.run(host=host, port=port, debug=debug)
+    print(f"Closing {host}:{port}")
