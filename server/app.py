db991636bd3c02f303980b52e603edb98fcedbce
czlonkowski <56956555+czlonkowski@users.noreply.github.com>
2023-05-06 00:32:38 +0200

    blabla
    blabla

--- server/app.py
+++ server/app.py
@@ -1,3 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__, template_folder='./../client/html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 1338))
+    app.run(host="0.0.0.0", port=port)
